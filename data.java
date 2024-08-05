79c043290e2d16badb15605371b83f6e26aecc19
KeyzaNadineKusumaRamadani <keyza_ramadani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:18 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,14 @@
+public class data {
+    public static void main(String[]args) {
+        //tipe_data nama_var = value/isi var;
+        String nama = "keyza";
+        int usia = 15;
+        String alamat = "Jl. Danau Towuti IV";
+        double kelas =10.7;
+
+        System.out.println("Nama saya :" + nama);
+        System.out.println("usia : " + usia + " tahun");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
