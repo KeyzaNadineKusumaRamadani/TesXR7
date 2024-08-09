0f533b0b7fbe2b84ca2c33575d93893ffca5760e
KeyzaNadineKusumaRamadani <keyza_ramadani_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:07:57 +0700

    Add files via upload

---
+++ Siswatester.java
@@ -0,0 +1,76 @@
+// Driver class
+public class Siswatester {
+    public static void main(String[] args) {
+    //  Object
+    // Class object = new constructor 
+        Siswa nisa = new Siswa();  
+        Siswa ria = new Siswa();
+        Siswa adi = new Siswa();
+        Siswa angga = new Siswa();
+        Siswa dion = new Siswa();
+        
+        angga.id = 5;
+        nisa.nama = "Annisa";
+        ria.ipk = 90;
+
+        System.out.println("Nama : "+adi.nama);
+        System.out.println(nisa.nama);
+
+
+        // Object
+        // Class object = new constructor
+        Siswa aveline = new Siswa();
+        Siswa memey = new Siswa();
+        Siswa kalila = new Siswa ();
+        Siswa keyza = new Siswa ();
+        Siswa leni = new Siswa ();
+
+
+        aveline.id = 4;
+        aveline.nama = "aveline voleta wardani";
+        aveline.ipk = 90;
+
+        memey.id = 31;
+        memey.nama = "nur azlina mayla sari";
+        memey.ipk = 90;
+
+        kalila.id = 12;
+        kalila.nama = "kalilah raihanna rizky arafah";
+        kalila.ipk = 90;
+
+        keyza.id = 14;
+        keyza.nama = "keyza nadine kusuma ramadani ";
+        keyza.ipk = 90;
+
+        leni.id = 18;
+        leni.nama = "leni sintiasari";
+        leni.ipk = 90;
+
+        
+        System.out.println("id :" + aveline.id);
+        System.out.println("nama :" + aveline.ipk);
+        System.out.println("ipk" + aveline.ipk);
+        
+        System.out.println("id :"  + memey.id);
+        System.out.println("nama :" + memey.nama);
+        System.out.println("ipk :" + memey.ipk);
+
+        System.out.println("id :" + kalila.id);
+        System.out.println("nama :" + kalila.nama);
+        System.out.println("ipk :" + kalila.ipk);
+
+        System.out.println("id :" + keyza.id);
+        System.out.println("nama :" + keyza.nama);
+        System.out.println("ipk :" +keyza.ipk);
+
+        System.out.println("id:" + leni.id);
+        System.out.println("nama :" + leni.nama);
+        System.out.println("ipk :" + leni.ipk);
+        }
+             
+    
+    }
+
+
+
+
